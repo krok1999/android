d459ca567cb93fe50b5ae97312ea8884d386b961
krok1999 <molochko.andrey@mail.ru>
2018-02-03 18:43:10 +0300

    addition frameworks

--- app/src/main/java/com/example/user/forgit/MainActivity.java
+++ app/src/main/java/com/example/user/forgit/MainActivity.java
@@ -2,16 +2,37 @@ package com.example.user.forgit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
+import butterknife.BindString;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class MainActivity extends AppCompatActivity {
-    TextView textView;
     //git remote add origin https://github.com/krok1999/android.git
+    @BindView(R.id.button)Button btn1;
+    @BindView(R.id.button2)Button btn2;
+    @BindView(R.id.text)TextView textView;
+    @BindString(R.string.hello)String str;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView = (TextView)findViewById(R.id.text);
+        ButterKnife.bind(this);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(view.getId()== btn2.getId())textView.setText("change");
+            }
+        });
+    }
+    @OnClick({R.id.button})
+    public void someMethod(){
+        btn1.setText("you clicked");
     }
 }
--- app/src/test/java/com/example/user/forgit/ExampleUnitTest.java
+++ app/src/test/java/com/example/user/forgit/ExampleUnitTest.java
@@ -1,7 +1,19 @@
 package com.example.user.forgit;
 
-import org.junit.Test;
+import android.content.res.Resources;
 
+import org.hamcrest.core.Is;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 /**
@@ -9,9 +21,23 @@ import static org.junit.Assert.*;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+
+@RunWith(MockitoJUnitRunner.class)
 public class ExampleUnitTest {
+
+
+
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
     }
+
+    @Test(expected = NullPointerException.class)
+    public void chechOnEmptyString(){
+        String str = null;
+        assertTrue(str.isEmpty());
+    }
+
+
+
 }
